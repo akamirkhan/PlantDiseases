38d0c40dee25c504de33e232d8772d6a084c5c95
Amirkhan <aak82183@gmail.com>
2024-01-11 11:24:01 +0300

    Initial Commit

---
+++ PlantDiseases/PlantDiseasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantDiseasesApp.swift
+//  PlantDiseases
+//
+//  Created by Amirkhan Akaev on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlantDiseasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
